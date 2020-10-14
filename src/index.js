b45d35845d2612d347f645061cb6222ff9eed30a
Laura <laura@Macbook-Laura.local>
2020-10-14 14:02:00 +0200

    pdf report diagnosis

--- src/index.js
+++ src/index.js
@@ -1,33 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { PDFViewer, PDFDownloadLink } from '@react-pdf/renderer';
-import { Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
+import { Document, Page, Text, View, StyleSheet, Image } from '@react-pdf/renderer';
 import './index.css';
-
+import dinbeatlogo from './img/logo.png';
 
 //tutorial
 // https://codesandbox.io/s/react-pdf-demo-i1ted?from-embed=&file=/src/MovieList.jsx
 
 const styles = StyleSheet.create({
   page: {
-    flexDirection: 'row',
-    backgroundColor: '#E4E4E4'
+    flexDirection: 'column',
+    backgroundColor: '#f0f3f7'
   },
-  section: {
-    margin: 10,
+  logo: {
+    width: 150,
     padding: 10,
-    flexGrow: 1
-  }
+    left: 430,
+  },
+  clinicName: {
+
+  },
+  divPaciente: {
+    backgroundColor: '#ebebee'
+  },
+  datosPaciente: {
+    color: '#2C3B4D'
+  },
+
 });
 
 const MyDocument = () => (
   <Document>
     <Page size="A4" style={styles.page}>
-      <View style={styles.section}>
-        <Text>Section #1</Text>
+    <Image src={dinbeatlogo} alt="images"  style={styles.logo} />
+      <View style={styles.title}>
+        <Text style={styles.clinicName}>NOMBRE CLINICA DIAGNOSTICADORA</Text>
       </View>
-      <View style={styles.section}>
-        <Text>Section #2</Text>
+      <View style={styles.divPaciente}>
+        <Text style={styles.datosPaciente}>DATOS PACIENTE</Text>
+        <View style={{flexDirection: 'row'}}>
+          <View>
+              <Text>Nombre</Text>
+              <Text>Sexo</Text>
+              <Text>Raza</Text>
+              <Text>Peso</Text>
+              <Text>Fecha de nacimiento</Text>
+          </View>
+          <View>
+              <Text>Clínica de origen</Text>
+              <Text>Fecga demanda del servicio</Text>
+              <Text>Motivo del exámen</Text>
+          </View>
+        </View>
       </View>
       <View style={styles.section}>
         <Text>Section #3</Text>
